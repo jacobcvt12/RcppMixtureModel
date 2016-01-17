2fcf4ea0674e13de2998e8ad14cf5daec7eda6ff
Jacob Carey <jacobcvt12@gmail.com>
2016-01-17 17:56:28 -0500

    Print out summary of parameters

--- R/mcmc.R
+++ R/mcmc.R
@@ -6,6 +6,48 @@ run.model <- function(data, k=2, thin=0, burnin=1000, sample=1000, cores=3) {
 
     mcmc.out <- run_model(data, k, thin, burnin, sample, cores)
 
+    # calculate summary statistics
+    theta <- mcmc.out$theta
+    sigma <- mcmc.out$sigma
+
+    # matrix for "pretty printing"
+    mcmc.summary <- matrix(0, nrow=(k * 2), ncol=4)
+    colnames(mcmc.summary) <- c("2.5%", "Mean", "97.5%", "R.hat")
+    rownames(mcmc.summary) <- c(paste("theta", 1:3), paste("sigma", 1:3))
+
+    for (i in 1:k) {
+        # concatenate chains
+        theta.all <- as.vector(theta[, i, ])
+        sigma.all <- 1 / as.vector(sigma[, i, ])
+
+        if (cores > 1) {
+            # calculate diagnostic
+            theta.rhat <- round(gelman.rubin(theta[, i, ]), 2)
+            sigma.rhat <- round(gelman.rubin(sigma[, i, ]), 2)
+        } else {
+            theta.rhat <- NA
+            sigma.rhat <- NA
+        }
+
+        # calculate mean summaries
+        theta.mean <- mean(theta.all)
+        theta.2.5 <- quantile(theta.all, 0.025)
+        theta.97.5 <- quantile(theta.all, 0.975)
+
+        # calculate variance summaries
+        sigma.mean <- mean(sigma.all)
+        sigma.2.5 <- quantile(sigma.all, 0.025)
+        sigma.97.5 <- quantile(sigma.all, 0.975)
+
+        # store in matrix
+        mcmc.summary[paste("theta", i), ] <- c(theta.2.5, theta.mean,
+                                               theta.97.5, theta.rhat)
+        mcmc.summary[paste("sigma", i), ] <- c(sigma.2.5, sigma.mean,
+                                               sigma.97.5, sigma.rhat)
+    }
+
+    print(mcmc.summary)
+
     return(mcmc.out)
 }
 
