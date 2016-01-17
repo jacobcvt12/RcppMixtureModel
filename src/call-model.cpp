5194737d1cc337e2f44e70df1538cb91f578b033
Jacob Carey <jacobcvt12@gmail.com>
2016-01-17 07:55:31 -0500

    Add exported wrapper for C++ cod

---
+++ src/call-model.cpp
@@ -0,0 +1,15 @@
+#include <Rcpp.h>
+#include "MixtureModel.hpp"
+
+// [[Rcpp::export]]
+Rcpp::List run_model(arma::vec data, unsigned int k,
+                     arma::vec sigma, arma::vec lambda, arma::ivec z,
+                     unsigned int burnin, unsigned int sample) {
+    MixtureModel model(data, k, sigma, lambda, z, burnin, sample);
+
+    // run MCMC
+    model.run_burnin();
+    model.posterior_sample();
+
+    return model.get_chains();
+}
