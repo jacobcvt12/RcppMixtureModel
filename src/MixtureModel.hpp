237ac301d1378139b146cb1e485be10b6260c2cb
Jacob Carey <jacobcvt12@gmail.com>
2016-01-16 19:27:38 -0500

    Add more variables to class

--- src/MixtureModel.hpp
+++ src/MixtureModel.hpp
@@ -6,14 +6,24 @@
 
 class MixtureModel {
   private:
+    // MCMC parameters
     unsigned int _nBurn;     // number of burnin iterations
     unsigned int _nSample;   // number of posterior samples
     unsigned int _k;         // number of components
+
+    // data
     arma::vec _data;
 
-    void update_mu();
-    void update_lambda();
+    // parameters
+    arma::vec _theta;       // means
+    arma::vec _sigma;       // variances
+    arma::vec _lambda;      // proportions
+    arma::vec _z;           // latent assignment
+
+    void update_theta();
     void update_sigma();
+    void update_lambda();
+    void update_z();
 
   public:
     MixtureModel(arma::vec data, unsigned int k,
