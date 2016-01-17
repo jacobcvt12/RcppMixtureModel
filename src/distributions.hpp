4262b315a072d3a925278260d4d597a4673056bd
Jacob Carey <jacobcvt12@gmail.com>
2016-01-16 21:02:11 -0500

    Remove default parameters from distribution

--- src/distributions.hpp
+++ src/distributions.hpp
@@ -6,8 +6,8 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 
 arma::vec rnorm(int n=1, double mean=0.0, double variance=1.0);
-arma::vec rdirichlet(int n=1, arma::vec alpha);
-std::vector<int> rmultinom(int n=1, arma::vec p);
-std::vector<int> rz(unsigned int n=1, unsigned int k);
+arma::vec rdirichlet(int n, arma::vec alpha);
+std::vector<int> rmultinom(int n, arma::vec p);
+std::vector<int> rz(unsigned int n, unsigned int k);
 
 #endif
